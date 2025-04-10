4dd238ca632ac51e6dfe724bce46ae682a180497
chrometano <tanoc082@gmail.com>
2025-04-10 18:36:21 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'chromic',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "chrome tano tech",
+  botOwner: 'chrome tano', // Your name
+  ownerNumber: '254792729056', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
